81a2bdcc1f449407375555ded4c34eddd347b87b
kana-328 <kana-328@i.softbank.jp>
2020-10-30 20:50:44 +0900

    InputEvent設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { FormControl, TextField } from "@material-ui/core";
 import './App.css';
 import { db } from "./firebase"
+import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
 
   const App: React.FC = () => {
     const [tasks, setTasks] = useState([{id: "", title:""}]);
+    const [input, setInput] = useState("");
     useEffect(() => {
       const unSub = db.collection("tasks").onSnapshot((snapshot) => {
         setTasks(
@@ -12,8 +15,29 @@ import { db } from "./firebase"
       });
       return () => unSub();
     }, []);
+
+    const newTask = (e: React.MouseEvent<HTMLButtonElement>) => {
+      db.collection("tasks").add({title: input});
+      setInput("")
+    }
     return <div className="App">
-      {tasks.map((task)=><h3>{task.title}</h3>)}
+      <h1>Todo App by React/Firebase</h1>
+      <FormControl>
+        <TextField 
+        InputLabelProps={{
+          shrink: true,
+        }}
+        label="New task ?"
+        value={input}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => 
+          setInput(e.target.value)
+        } 
+        />
+      </FormControl>
+      <button disabled={!input} onClick={newTask}>
+        <AddToPhotosIcon />
+      </button>
+      {tasks.map((task)=><h3 key={task.id}>{task.title}</h3>)}
     </div>
   };
 
---
+++ src/TaskItem.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as firebase from "firebase/app";
+import { ListItem, TextField, Grid } from "@material-ui/core";
+
+export const TaskItem = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
